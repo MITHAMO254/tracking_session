83f3fdbd8c7492cf8bffd4b6de7d81c67ed8f557
chris <mithamochris94@gmail.com>
2020-06-10 14:32:28 +0300

    changed the user interface for my system

---
+++ session.php
@@ -0,0 +1,24 @@
+<?php
+include 'tracking.php';
+include '../metavisuo/v1.0/sql_library.php';
+$tracking = new tracking_assignment();
+$sql = new sql\editor('session', 'tracking_assignment');
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="developer.css">
+    </head>
+    <body>
+        <?php
+        $sql->show('tracking_assignment');
+        ?>
+    </body>
+</html>
+
+
+
+
+
+
